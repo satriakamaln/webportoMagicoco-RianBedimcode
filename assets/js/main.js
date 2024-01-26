7d18cff6b0facc3902a7182fd8f7915b30dfe0b2
Satria Kamal Norsaid <satriakamaln@gmail.com>
2024-01-26 10:30:51 +0800

    add scroll active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,7 +85,31 @@ const scrollUp = () => {
 window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id"),
+            sectionClass = document.querySelector(
+                ".nav__menu a[href*=" + sectionId + "]"
+            );
+
+        if (
+            scrollDown > sectionTop &&
+            scrollDown <= sectionTop + sectionHeight
+        ) {
+            sectionClass.classList.add("active-link");
+        } else {
+            sectionClass.classList.remove("active-link");
+        }
+    });
+};
+
+window.addEventListener("scroll", scrollActive);
 /*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/

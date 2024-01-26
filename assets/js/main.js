abecc9e072d73b5ca3a0c4039ed6e799c7011323
Satria Kamal Norsaid <satriakamaln@gmail.com>
2024-01-26 10:13:21 +0800

    add webkit&scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,6 +76,14 @@ const sendEmail = (e) => {
 contactForm.addEventListener("submit", sendEmail);
 /*=============== SHOW SCROLL UP ===============*/
 
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up");
+    this.scrollY >= 350
+        ? scrollUp.classList.add("show-scroll")
+        : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== DARK LIGHT THEME ===============*/
